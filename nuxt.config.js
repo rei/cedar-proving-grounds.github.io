8cba8fc407ec66989398071540e9d8904503dd95
vagrant <zfolwic@rei.com>
2018-09-14 11:34:50 -0700

    x

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,8 +10,8 @@ module.exports = {
   /*
    ** Toggle for client side vs. server side rendering.
    */
-  mode: 'universal',
-  //mode: 'spa',
+  //mode: 'universal',
+  mode: 'spa',
   /*
   ** Headers of the page
   */

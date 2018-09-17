8a1331b081b9dab340495716f5708cb47bbd7d31
vagrant <zfolwic@rei.com>
2018-09-17 16:12:01 -0700

    spa

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,29 +1,53 @@
 <template>
   <div>
-    <!--<p>Cedar Proving Grounds default.vue</p>-->
-    <!--<router-link-->
-      <!--v-for="route in routes"-->
-      <!--:key="route.name"-->
-      <!--:to="route.path">{{ route.name }} *-->
-    <!--</router-link>-->
-
-    <!--<router-view></router-view>-->
-    <!--<component v-for="section of data" :key="section.id" :is="section.name" />-->
-    <nuxt />
+    <nuxt/>
   </div>
 </template>
 
-<!--<script>-->
-  <!--import routes from '~/routes';-->
-
-  <!--export default {-->
-    <!--name: 'landing',-->
-    <!--data() {-->
-      <!--return {-->
-        <!--routes,-->
-      <!--};-->
-    <!--},-->
-  <!--};-->
-
-<!--</script>-->
-
+<style>
+html
+{
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+*, *:before, *:after
+{
+  box-sizing: border-box;
+  margin: 0;
+}
+.button--green
+{
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #3b8070;
+  color: #3b8070;
+  text-decoration: none;
+  padding: 10px 30px;
+}
+.button--green:hover
+{
+  color: #fff;
+  background-color: #3b8070;
+}
+.button--grey
+{
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #35495e;
+  color: #35495e;
+  text-decoration: none;
+  padding: 10px 30px;
+  margin-left: 15px;
+}
+.button--grey:hover
+{
+  color: #fff;
+  background-color: #35495e;
+}
+</style>

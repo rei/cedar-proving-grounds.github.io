b3648f85c44e9afe618449350d72690b7f8635cf
vagrant <zfolwic@rei.com>
2018-09-20 13:45:56 -0700

    build

--- pages/KitchenSink.vue
+++ pages/KitchenSink.vue
@@ -6,40 +6,40 @@
 </template>
 
 <script>
-import Buttons from '~/pages/KitchenSink/components/Buttons'
+////import Buttons from '~/pages/KitchenSink/components/Buttons'
 import Cta from '~/pages/KitchenSink/components/Cta'
-import Links from '~/pages/KitchenSink/components/Links'
-import Breadcrumb from '~/pages/KitchenSink/components/Breadcrumb'
+////import Links from '~/pages/KitchenSink/components/Links'
+////import Breadcrumb from '~/pages/KitchenSink/components/Breadcrumb'
 // import Cards from 'componentsdir/card/examples/Cards'
 // import Checkbox from 'componentsdir/checkbox/examples/checkboxes'
 // import Grid from 'componentsdir/grid/examples/Grid'
 // import Checkboxes from 'componentsdir/checkbox/examples/checkboxes'
-import Texts from '~/pages/KitchenSink/components/Text'
-import Icons from '~/pages/KitchenSink/components/Icons'
-import Images from '~/pages/KitchenSink/components/Images'
-import Quotes from '~/pages/KitchenSink/components/Quote'
-import Accordion from '~/pages/KitchenSink/components/Accordion'
+////import Texts from '~/pages/KitchenSink/components/Text'
+////import Icons from '~/pages/KitchenSink/components/Icons'
+////import Images from '~/pages/KitchenSink/components/Images'
+////import Quotes from '~/pages/KitchenSink/components/Quote'
+////import Accordion from '~/pages/KitchenSink/components/Accordion'
 // import Forms from 'componentsdir/input/examples/Inputs'
 // import Lists from 'componentsdir/list/examples/Lists'
 // import MediaObject from 'componentsdir/mediaObject/examples/mediaObject'
 // import Rating from 'componentsdir/rating/examples/Ratings';
 // import Radios from 'componentsdir/radio/examples/Radios'
 // import Selects from 'componentsdir/select/examples/Selects'
-import CaptionExample from '~/pages/KitchenSink/components/Caption'
+////import CaptionExample from '~/pages/KitchenSink/components/Caption'
 // import Activity from 'compositionsdir/activityCard/examples/activity'
 // import Searchbox from 'compositionsdir/search/examples/searchbox'
-import Tabs from '~/pages/KitchenSink/components/Tabs'
+////import Tabs from '~/pages/KitchenSink/components/Tabs'
 
 export default {
   name: 'KitchenSink',
   components: {
-    Links,
-    Breadcrumb,
-    Accordion,
-    Buttons,
+////    Links,
+////    Breadcrumb,
+////    Accordion,
+////    Buttons,
     Cta,
     // Texts,
-    Icons,
+////    Icons,
     // Lists,
     // Selects,
     // MediaObject,
@@ -49,38 +49,38 @@ export default {
     // Grid,
     // Checkboxes,
     // Cards,
-    Images,
+////    Images,
     // Searchbox,
-    Quotes,
-    CaptionExample,
+////    Quotes,
+////    CaptionExample,
     // Activity,
-    Tabs
+////    Tabs
   },
   data() {
     return {
       data: [
-        Links,
-        Breadcrumb,
-        Accordion,
-        Buttons,
+////        Links,
+////        Breadcrumb,
+////        Accordion,
+////        Buttons,
         Cta,
         // Cards,
         // Activity,
         // Checkboxes,
         // Grid,
         // Texts,
-        Icons,
-        Images,
+////        Icons,
+////        Images,
         // Searchbox,
         // Forms,
         // Lists,
         // MediaObject,
         // Radios,
-        Quotes,
+////        Quotes,
         // Rating,
         // Selects,
-        CaptionExample,
-        Tabs
+////        CaptionExample,
+////        Tabs
       ]
     }
   }

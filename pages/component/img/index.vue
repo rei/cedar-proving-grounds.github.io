3f0d36b61927b71c0f529ae62fc1f20349137ae4
vagrant <zfolwic@rei.com>
2018-09-24 09:40:19 -0700

    build

---
+++ pages/component/img/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h2>Images</h2>
+    <div data-backstop="image-aspect-ratio">
+      <h3>Aspect Ratios (with landscape images)</h3>
+      <ratios/>
+    </div>
+
+    <div data-backstop="image-cropping">
+      <cropping/>
+    </div>
+
+    <div data-backstop="image-mods">
+      <mods/>
+    </div>
+
+    <div data-backstop="image-standard">
+      <h3>Standard image</h3>
+      <cdr-img
+        alt="ratio standard"
+        src="http://placehold.it/200x200"
+      />
+      <cdr-img
+        alt="standard landscape"
+        src="http://placehold.it/350x150"
+      />
+      <cdr-img
+        alt="standard portrait"
+        src="http://placehold.it/150x350"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import { CdrImg } from '~/cedar';
+import ratios from './Ratios';
+import cropping from './Cropping';
+import mods from './Mods';
+
+export default {
+  name: 'Images',
+  components: CdrImg,
+};
+</script>
---
+++ pages/component/quote/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h2>Quote version: {{ version }}</h2>
+    <div class="quotes-examples">
+      <blockquotes />
+
+      <pullquotes />
+    </div>
+  </div>
+</template>
+
+<script>
+import Blockquotes from './Blockquotes';
+import Pullquotes from './Pullquotes';
+const deps = require('~/package').dependencies;
+
+export default {
+  name: 'Quotes',
+  components: {
+    Blockquotes,
+    Pullquotes,
+  },
+  data() {
+    return {
+      version: deps["@rei/cdr-quote"],
+    };
+  },
+};
+</script>
+
+<style>
+  .quotes {
+    padding: 50px
+  }
+</style>
